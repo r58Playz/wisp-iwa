a724635c94961ff3252da0cca2f576fa60b483b6
Toshit Chawda <r58playz@gmail.com>
2024-09-24 18:47:37 -0700

    initial commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite';
 import { dreamlandPlugin } from 'vite-plugin-dreamland';
 
+import fs from 'fs';
+import wbn from 'rollup-plugin-webbundle';
+import * as wbnSign from 'wbn-sign';
+
+const plugins = [dreamlandPlugin()];
+
+if (process.env.NODE_ENV === 'production') {
+	const key = wbnSign.parsePemKey(
+		Buffer.from(process.env.KEY || fs.readFileSync('./certs/encrypted_key.pem')),
+		process.env.KEY_PASSPHRASE ||
+		(await wbnSign.readPassphrase(
+			'./certs/encrypted_key.pem',
+		)),
+	);
+
+	plugins.push({
+		...wbn({
+			baseURL: new wbnSign.WebBundleId(key).serializeWithIsolatedWebAppOrigin(),
+			static: {
+				dir: 'public',
+			},
+			output: 'wisp-iwa.swbn',
+			integrityBlockSign: {
+				strategy: new wbnSign.NodeCryptoSigningStrategy(key),
+			},
+		}),
+		enforce: 'post',
+	});
+}
+
 export default defineConfig({
-    plugins: [dreamlandPlugin()],
-    base: './'
+	plugins,
+	server: {
+		port: 5193,
+		strictPort: true,
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost',
+			clientPort: 5193,
+		},
+		headers: {
+			"permissions-policy": "direct-sockets"
+		}
+	},
+	base: "./",
 });
