9274c4ba3525074f209be070a6727b1cae5c48fd
Toshit Chawda <r58playz@gmail.com>
2024-10-19 19:30:59 -0700

    update rust

--- rust/build.sh
+++ rust/build.sh
@@ -1,3 +1,9 @@
+WBG="wasm-bindgen 0.2.95"
+if [ "$(wasm-bindgen -V)" != "$WBG" ]; then
+	echo "Incorrect wasm-bindgen version: '$(wasm-bindgen -V)' != '$WBG'"
+	exit 1
+fi
+
 mkdir pkg
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' cargo build --target wasm32-unknown-unknown -Z build-std=panic_abort,std --release "$@"
 wasm-bindgen --target web --out-dir pkg/ target/wasm32-unknown-unknown/release/wisp_iwa_rust.wasm
